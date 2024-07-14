034b8792fdfb36ad2256071449158b156ea1e7c2
Isaac <zaheer@avatarmc.com>
2024-07-14 16:49:00 +0100

    New Hamburger Menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Logo from "./Logo";
 import NavOption from "./NavOption";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { gsap } from "gsap";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -11,38 +10,69 @@ const Navbar = () => {
     setMenuOpen(!menuOpen);
   };
 
+  useEffect(() => {
+    if (menuOpen) {
+      gsap.to(".navbar", {
+        x: 0,
+        duration: 0.5,
+        ease: "power2.out",
+        opacity: 1,
+        display: "flex",
+      });
+      gsap.to(".nav-item", {
+        opacity: 1,
+        duration: 0.5,
+        delay: 0.5,
+        stagger: 0.1,
+      });
+    } else {
+      gsap.to(".nav-item", {
+        opacity: 0,
+        duration: 0.5,
+        stagger: 0.1,
+      });
+      gsap.to(".navbar", {
+        x: "-100%",
+        duration: 0.5,
+        ease: "power2.in",
+        opacity: 0,
+        display: "hidden",
+      });
+    }
+  }, [menuOpen]);
+
   return (
-    <nav className="bg-stone-200 bg-opacity-50 backdrop-blur-md px-4 py-4 flex justify-between items-center fixed top-0 left-0 w-full z-50">
+    <nav className="bg-stone-200 bg-opacity-50 backdrop-blur-md px-4 py-2 flex justify-between items-center fixed top-0 left-0 w-full z-50">
       <Logo />
       <div className="md:hidden relative">
-        <button onClick={toggleMenu} className="relative z-10">
-          <div
-            className={`mr-2 transition-opacity duration-300 ease-in-out transform ${
-              menuOpen ? "opacity-0 rotate-180" : "opacity-100 rotate-0"
-            }`}
-          >
-            <FontAwesomeIcon icon={faBars} size="2x" />
-          </div>
-          <div
-            className={`mr-2 absolute inset-0 transition-opacity duration-300 ease-in-out transform ${
-              menuOpen ? "opacity-100 rotate-0" : "opacity-0 -rotate-180"
-            }`}
-          >
-            <FontAwesomeIcon icon={faTimes} size="2x" />
-          </div>
+        <button
+          className={`hamburger hamburger--collapse z-50 ${
+            menuOpen ? "fixed right-4 top-2 is-active" : ""
+          }`}
+          type="button"
+          onClick={toggleMenu}
+          id="hamburger"
+        >
+          <span className="hamburger-box">
+            <span className="hamburger-inner"></span>
+          </span>
         </button>
       </div>
       <ul
-        className={`${
-          menuOpen ? "flex" : "hidden"
-        } flex-col md:flex md:flex-row space-y-4 md:space-y-0 md:space-x-8 ${
-          menuOpen ? "bg-stone-200 bg-opacity-90 backdrop-blur-md" : ""
-        } absolute md:relative top-16 left-0 right-0 md:top-auto md:left-auto md:right-auto md:bg-transparent transition-transform transform ${
+        className={`navbar flex-col md:flex md:flex-row space-y-4 md:space-y-0 md:space-x-8 ${
+          menuOpen &&
+          "bg-stone-200 bg-opacity-95 backdrop-blur-md h-[100vh] w-full animate-navbarSlideIn z-40 items-center justify-center no-scroll"
+        } absolute md:relative top-0 left-0 right-0 md:top-auto md:left-auto md:right-auto md:bg-transparent transition-transform transform ${
           menuOpen ? "translate-y-0" : "-translate-y-full"
         } md:translate-y-0 md:transform-none w-full md:w-auto`}
       >
         {["home", "about", "projects", "contact"].map((text) => (
-          <li key={text} className="md:pl-0 md:pb-0 pt-2 pl-4 pb-2">
+          <li
+            key={text}
+            className={`nav-item opacity-0 md:pl-0 md:pb-0 pt-2 pl-4 pb-2 ${
+              menuOpen ? "text-2xl" : ""
+            }`}
+          >
             <NavOption text={text} />
           </li>
         ))}
