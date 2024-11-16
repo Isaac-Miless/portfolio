123e10974be3a001a34377b13468f879a72d78ba
Isaac <isaacmiles36@gmail.com>
2024-11-16 21:49:23 +0000

    Front-End Developer -> Software Developer

--- src/components/about-me/AboutMeText.jsx
+++ src/components/about-me/AboutMeText.jsx
@@ -8,8 +8,8 @@ function AboutMeText() {
         <Header title="ABOUT ME" />
       </div>
       <div className="about-me-txt mt-8 text-xl lg:text-2xl text-neutral text-center max-w-[70vw] md:min-w-[40vw] lg:max-w-[20vw]">
-        Hello! I'm <span className="text-primary">Isaac</span>, a front-end
-        developer and Software Engineering student at the University of Kent. My
+        Hello! I'm <span className="text-primary">Isaac</span>, a Software
+        Developer and Software Engineering student at the University of Kent. My
         passion is creating applications that are both functional and visually
         appealing.
         <br />
